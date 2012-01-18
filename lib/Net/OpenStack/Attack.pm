ef99537a986f566718703b6aac321e7407b8300c
Naveed Massjouni <naveed@vt.edu>
2012-01-18 06:20:41 +0000

    Import of IRONCAMEL/Net-OpenStack-Attack-1.0100 from CPAN.
    gitpan-cpan-distribution: Net-OpenStack-Attack
    gitpan-cpan-version:      1.0100
    gitpan-cpan-path:         IRONCAMEL/Net-OpenStack-Attack-1.0100.tar.gz
    gitpan-cpan-author:       IRONCAMEL
    gitpan-cpan-maturity:     released

--- lib/Net/OpenStack/Attack.pm
+++ lib/Net/OpenStack/Attack.pm
@@ -1,6 +1,68 @@
 package Net::OpenStack::Attack;
+use v5.10;
+use Any::Moose;
+use HTTP::Async;
+use HTTP::Request;
+use JSON qw(to_json from_json);
 
-our $VERSION = '1.0001'; # VERSION
+our $VERSION = '1.0100'; # VERSION
+
+has compute => (is => 'ro', isa => 'Net::OpenStack::Compute', required => 1);
+
+sub create_servers {
+    my ($self, $num, $image) = @_;
+    $image ||= $self->get_any_image();
+    my $body = to_json {
+        server => {
+            name      => 'stackattack',
+            imageRef  => $image,
+            flavorRef => 1,
+        }
+    };
+    my @reqs = map $self->make_req(POST => '/servers', $body), 1 .. $num;
+    return $self->send_reqs(@reqs);
+}
+
+sub delete_servers {
+    my ($self, $servers) = @_;
+    my @reqs = map { $self->make_req(DELETE => "/servers/$_->{id}") } @$servers;
+    return $self->send_reqs(@reqs);
+}
+
+sub attack {
+    my ($self, $method, $resource, $num) = @_;
+    my @reqs = map $self->make_req($method => $resource), 1 .. $num;
+    return $self->send_reqs(@reqs);
+}
+
+sub make_req {
+    my ($self, $method, $resource, $body) = @_;
+    my $url = $self->compute->base_url . $resource;
+    my $headers = [
+        x_auth_token => $self->compute->token,
+        content_type => 'application/json',
+    ];
+    return HTTP::Request->new($method => $url, $headers, $body);
+}
+
+sub send_reqs {
+    my ($self, @reqs) = @_;
+    my $async = HTTP::Async->new;
+    $async->add(@reqs);
+    my ($successes, $failures, @errmsgs) = (0, 0);
+    while (my $res = $async->wait_for_next_response) {
+        if ($res->is_success) {
+            $successes++;
+        } else {
+            $failures++;
+            warn sprintf "Error: %s: %s", $res->status_line, $res->content;
+        }
+    }
+
+    return { successes => $successes, failures => $failures };
+}
+
+sub get_any_image { shift->compute->get_images(detail => 0)->[0]{id} }
 
 # ABSTRACT: Tools for stress testing an OpenStack deployment.
 
@@ -16,11 +78,24 @@ Net::OpenStack::Attack - Tools for stress testing an OpenStack deployment.
 
 =head1 VERSION
 
-version 1.0001
+version 1.0100
 
 =head1 DESCRIPTION
 
-You probably want to look at the documentation for L<stackattack> instead.
+This class provides methods for making parallel, asynchronous requests to
+the OpenStack API.
+This distribution comes with a command line tool L<stackattack> which heavily
+uses this class.
+
+=head1 SEE ALSO
+
+=over
+
+=item L<stackattack>
+
+=item L<Net::OpenStack::Compute>
+
+=back
 
 =head1 AUTHORS
 
