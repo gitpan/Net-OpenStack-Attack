0f1da8103b0895773c4f071b52005a0d62ba7afc
Naveed Massjouni <naveed@vt.edu>
2012-03-08 16:45:16 +0000

    Import of IRONCAMEL/Net-OpenStack-Attack-1.0300 from CPAN.
    gitpan-cpan-distribution: Net-OpenStack-Attack
    gitpan-cpan-version:      1.0300
    gitpan-cpan-path:         IRONCAMEL/Net-OpenStack-Attack-1.0300.tar.gz
    gitpan-cpan-author:       IRONCAMEL
    gitpan-cpan-maturity:     released

--- lib/Net/OpenStack/Attack.pm
+++ lib/Net/OpenStack/Attack.pm
@@ -5,7 +5,7 @@ use HTTP::Async;
 use HTTP::Request;
 use JSON qw(to_json from_json);
 
-our $VERSION = '1.0200'; # VERSION
+our $VERSION = '1.0300'; # VERSION
 
 has compute => (is => 'ro', isa => 'Net::OpenStack::Compute', required => 1);
 
@@ -78,7 +78,7 @@ Net::OpenStack::Attack - Tools for stress testing an OpenStack deployment.
 
 =head1 VERSION
 
-version 1.0200
+version 1.0300
 
 =head1 DESCRIPTION
 
